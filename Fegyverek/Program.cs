9e487cd6e9f2307f757222b93ee83a12ca637c02
Körmöndi.Alex.István <Krmndi.Alex.Istvn@C102-05>
2020-02-25 13:17:32 +0100

    Kész a Fegyvertár.

--- Fegyverek/Program.cs
+++ Fegyverek/Program.cs
@@ -8,9 +8,18 @@ namespace Fegyverek
 {
     class Program
     {
+        enum fegyverek {apaökle, uzi, lángszoró, kézigránát};
         static void Main(string[] args)
         {
-
+            fegyverek kezemben = fegyverek.kézigránát;
+            List<fegyverek> fegyvertár = new List<fegyverek>();
+            fegyvertár.Add(fegyverek.uzi);
+            fegyvertár.Add(fegyverek.lángszoró);
+            foreach (fegyverek elem in fegyvertár)
+            {
+                Console.WriteLine(elem);
+            }
+            Console.ReadKey();
         }
     }
 }
